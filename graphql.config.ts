05d823726438a3172071b94ddb6cc43ba9439c9c
Chris Sauve <chrismsauve@gmail.com>
2024-07-26 01:41:11 -0400

    Nest apps together

--- graphql.config.ts
+++ graphql.config.ts
@@ -3,17 +3,17 @@ import type {Configuration} from '@quilted/graphql-tools/configuration';
 const configuration: Configuration = {
   schema:
     'node_modules/@lemonmade/shopify/graphql/2024-04/storefront.schema.graphql',
-  documents: ['graphql/**/*.graphql', 'multi-page/**/*.graphql'],
+  documents: ['app/**/*.graphql'],
   extensions: {
     quilt: {
       schema: [
         {
           kind: 'definitions',
-          outputPath: 'graphql/graphql/storefront.schema.ts',
+          outputPath: 'app/graphql/graphql/storefront.schema.ts',
         },
         {
           kind: 'definitions',
-          outputPath: 'multi-page/graphql/storefront.schema.ts',
+          outputPath: 'app/multi-page/graphql/storefront.schema.ts',
         },
       ],
     },
