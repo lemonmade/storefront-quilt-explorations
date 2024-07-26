21586f89ca893da30525a1c8fd344678db22ac97
Chris Sauve <chrismsauve@gmail.com>
2024-07-25 22:26:16 -0400

    Fix last type errors

--- app/browser/graphql.tsx
+++ app/browser/graphql.tsx
@@ -1,3 +1,4 @@
+// @ts-expect-error
 if (process.env.NODE_ENV === 'development') {
   await import('preact/debug');
 }
--- app/tests/graphql.ts
+++ app/tests/graphql.ts
@@ -4,10 +4,11 @@ import {
   createGraphQLSchema,
   createGraphQLFiller,
 } from '@quilted/quilt/graphql/testing';
+import type {GraphQLSchema} from 'graphql';
 
 import schemaSource from '../graphql/storefront.schema.ts';
 
-export const schema = createGraphQLSchema(schemaSource);
+export const schema: GraphQLSchema = createGraphQLSchema(schemaSource);
 export const fillGraphQL = createGraphQLFiller(schema);
 
 export {GraphQLController, GraphQLTesting};
