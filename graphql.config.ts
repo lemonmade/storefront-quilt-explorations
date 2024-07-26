7357aa1f931ace1f9f2794988c3c6fb986f578e5
Chris Sauve <chrismsauve@gmail.com>
2024-07-26 01:33:08 -0400

    Rename app => graphql

--- graphql.config.ts
+++ graphql.config.ts
@@ -3,11 +3,14 @@ import type {Configuration} from '@quilted/graphql-tools/configuration';
 const configuration: Configuration = {
   schema:
     'node_modules/@lemonmade/shopify/graphql/2024-04/storefront.schema.graphql',
-  documents: ['app/**/*.graphql'],
+  documents: ['graphql/**/*.graphql', 'multi-page/**/*.graphql'],
   extensions: {
     quilt: {
       schema: [
-        {kind: 'definitions', outputPath: 'app/graphql/storefront.schema.ts'},
+        {
+          kind: 'definitions',
+          outputPath: 'graphql/graphql/storefront.schema.ts',
+        },
         {
           kind: 'definitions',
           outputPath: 'multi-page/graphql/storefront.schema.ts',
