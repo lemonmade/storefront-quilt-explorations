4bbd5aa2777c1eaf46b692479dec83a9e17c943e
Chris Sauve <chrismsauve@gmail.com>
2024-07-28 10:58:25 -0400

    Try deploying to cloudflare

--- app/multi-page/rollup.config.js
+++ app/multi-page/rollup.config.js
@@ -1,6 +1,13 @@
 import {quiltApp} from '@quilted/rollup/app';
+import {cloudflarePages} from '@quilted/cloudflare/craft';
 
-export default quiltApp({
-  browser: {entry: './browser.tsx', assets: {minify: false}},
+const config = await quiltApp({
+  browser: {
+    entry: './browser.tsx',
+    assets: {minify: false},
+  },
   server: {entry: './server.tsx'},
+  runtime: cloudflarePages(),
 });
+
+export default config;
